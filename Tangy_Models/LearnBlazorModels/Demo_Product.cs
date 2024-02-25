c16ebf148b2903325b7c6ea892b8b415acc20366
Daria Kyselytsia <daria.kys1@gmail.com>
2024-02-26 00:06:52 +0200

    product-list and demo-product added

--- Tangy_Models/LearnBlazorModels/Demo_Product.cs
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -10,7 +10,10 @@ namespace Tangy_Models.LearnBlazorModels
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public double Price { get; set; }
         public bool IsActive { get; set; }
 
+        public List<Demo_ProductProp> ProductProperties { get; set; }
     }
 }
