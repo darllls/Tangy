abcf952fa1bd9584864d64eecfca8a7a4e932c69
Daria Kyselytsia <daria.kys1@gmail.com>
2024-03-07 19:02:18 +0200

    Add Product CRUD

---
+++ Tangy_DataAccess/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangy_DataAccess
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool ShopFavourites { get; set; }
+        public bool CustomerFavourites { get; set; }
+        public string Color { get; set; }
+        public string ImageUrl { get; set; }
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+    }
+}
